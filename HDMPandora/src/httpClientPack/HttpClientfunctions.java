681dd0ca84451d1e8727371189b1ee08dc11c4b7
HayEid2017 <33356257+HayEid2017@users.noreply.github.com>
2021-11-28 19:07:24 +0200

    first commit

---
+++ HDMPandora/src/httpClientPack/HttpClientfunctions.java
@@ -0,0 +1,46 @@
+package httpClientPack;
+
+import java.net.http.HttpHeaders;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class HttpClientfunctions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String urlStr="http://10.98.85.4:7003/nbi/api/devices?subscriberId=2359883@tedata.net.eg";
+		
+		HttpGet request = new HttpGet(urlStr);
+
+        // add request headers
+        request.addHeader("custom-key", "mkyong");
+        request.addHeader(HttpHeaders.USER_AGENT, "Googlebot");
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+             CloseableHttpResponse response = httpClient.execute(request)) {
+
+            // Get HttpResponse Status
+            System.out.println(response.getProtocolVersion());              // HTTP/1.1
+            System.out.println(response.getStatusLine().getStatusCode());   // 200
+            System.out.println(response.getStatusLine().getReasonPhrase()); // OK
+            System.out.println(response.getStatusLine().toString());        // HTTP/1.1 200 OK
+
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                // return it as a String
+                String result = EntityUtils.toString(entity);
+                System.out.println(result);
+            }
+
+        }
+		
+		
+
+	}
+
+}
